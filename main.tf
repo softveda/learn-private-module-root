92e1140b712c49cac08e7afc469603c158bb0428
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-07-29 16:57:09 +1000

    changed module path

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ provider "aws" {
 }
 
 module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
+  source  = "app.terraform.io/pratik-hc/s3-webapp/aws"
+  version = "1.0.0"
   name        = var.name
   region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+  prefix = var.prefix  
 }
